3cced06d4a29a52de32e623dc0167d1cb13f24c6
Selva Ganesh M <selvadev2k@gmail.com>
2023-01-15 19:39:29 +0530

    user model creation

---
+++ src/models/userModel.ts
@@ -0,0 +1,43 @@
+import mongoose, { Model } from "mongoose";
+
+interface IUser {
+    username: string;
+    email: string;
+    password:string;
+    image: string;
+    subscribers: number;
+    subscribedUsers: Array<string>;
+
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    username: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    email: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    subscribers: {
+        type: Number,
+        default: 0,
+    },
+    subscribedUsers: {
+        type: [String]
+    }
+}, {
+    timestamps: true
+})
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+export default userModel
