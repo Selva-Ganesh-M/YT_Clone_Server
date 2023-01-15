0f509a2aba5b925bdc29f22077283007f7e1d1b1
Selva Ganesh M <selvadev2k@gmail.com>
2023-01-15 19:53:31 +0530

    comment model prep

---
+++ src/models/commentModel.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+export interface IComment {
+    content: string
+    userId: string
+    videoId: string
+}
+
+const commentSchema = new mongoose.Schema<IComment>({
+    content: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        require: true,
+        type: String
+    },
+    videoId: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const commentModel = mongoose.model<mongoose.Schema<IComment>>("Comment", commentSchema)
+
+export default commentModel
