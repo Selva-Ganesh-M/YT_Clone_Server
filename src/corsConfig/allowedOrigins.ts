b214b007ab7e718a2b461b7fad5c119157b43139
Selva Ganesh M <selvadev2k@gmail.com>
2023-02-22 21:50:18 +0530

    updated the hosted react app origin with cors

--- src/corsConfig/allowedOrigins.ts
+++ src/corsConfig/allowedOrigins.ts
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    "http://localhost:5173",
-]
+  "http://localhost:5173",
+  "https://ytub-clone.netlify.app",
+];
 
-export default allowedOrigins
+export default allowedOrigins;
